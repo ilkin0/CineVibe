c08be976e94351e0556058b271c270bb597c6c86
Ilkin Mehdiyev <hey@ilkinmehdiyev.com>
2025-01-19 10:17:09 +0200

    Init commit

---
+++ src/main/java/com/be001/cinevibe/CinevibeApplication.java
@@ -0,0 +1,13 @@
+package com.be001.cinevibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinevibeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinevibeApplication.class, args);
+    }
+
+}
