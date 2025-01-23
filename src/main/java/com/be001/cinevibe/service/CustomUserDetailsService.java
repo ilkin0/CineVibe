7cc855801dce49dfeb4a74f80d2f3d0e7d3d6e75
Hümbət Camalov <143912059+Camalzadeh@users.noreply.github.com>
2025-01-23 12:46:13 +0400

    some commit

--- src/main/java/com/be001/cinevibe/service/CustomUserDetailsService.java
+++ src/main/java/com/be001/cinevibe/service/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.be001.cinevibe.service;
 
+import com.be001.cinevibe.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,8 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userService.findByUsername(username)
-                .orElseThrow()
-                .userDetails();
+        User user = userService.findByUsername(username);
+        return user.userDetails();
     }
 }
--- src/main/java/com/be001/cinevibe/service/UserService.java
+++ src/main/java/com/be001/cinevibe/service/UserService.java
@@ -2,19 +2,27 @@ package com.be001.cinevibe.service;
 
 import com.be001.cinevibe.model.User;
 import com.be001.cinevibe.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
+    Logger logger = Logger.getLogger(UserService.class.getName());
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public User findByUsername(String username) {
+        return userRepository
+                .findByUsername(username).orElseThrow(() -> {
+                    logger.severe("User with username " + username + " not found");
+                    return new UsernameNotFoundException("User not found");
+                }
+                );
     }
 }
