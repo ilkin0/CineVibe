ffa3951e1c25a407bb365f396edf079c44439db5
hasanbadalli <hasanoaga2005@gmail.com>
2025-01-24 14:59:13 +0400

    jwt service added, application-security.properties added

--- src/main/java/com/be001/cinevibe/service/JwtService.java
+++ src/main/java/com/be001/cinevibe/service/JwtService.java
@@ -1,20 +1,40 @@
 package com.be001.cinevibe.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.Objects;
 
 @Service
 public class JwtService {
+    @Value("${security.jwt.accessToken.secret}")
+    private String accessTokenSecret;
+    @Value("${security.jwt.refreshToken.secret}")
+    private String refreshTokenSecret;
 
-    public String generateToken(String email, String secret, long expirationMS) {
+    @Value("${security.jwt.accessToken.expiration}")
+    private long accessTokenExpiration;
+    @Value("${security.jwt.refreshToken.expiration}")
+    private long refreshTokenExpiration;
+
+    public String generateAccessToken(String username) {
+        return generateToken(username, accessTokenSecret, accessTokenExpiration);
+    }
+
+    public String generateRefreshToken(String username) {
+        return generateToken(username, refreshTokenSecret, refreshTokenExpiration);
+    }
+
+    public String generateToken(String username, String secret, long expirationMS) {
         long currentTime = System.currentTimeMillis();
         return Jwts.builder()
-                .subject(email)
+                .subject(username)
                 .issuedAt(new Date(currentTime))
                 .expiration(new Date(currentTime + expirationMS))
                 .signWith(getSecretKey(secret))
@@ -24,5 +44,37 @@ public class JwtService {
     private SecretKey getSecretKey(String secret) {
         byte[] bytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(bytes);
+    }
+
+    public boolean isValidToken(String token, boolean isAccessToken) {
+        try {
+            Claims claims = extractClaims(token, isAccessToken);
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Date extractExpirationDate(String token, boolean isAccessToken) {
+        Claims claims = extractClaims(token, isAccessToken);
+        return claims.getExpiration();
+    }
+
+    public String extractUsername(String token, boolean isAccessToken) {
+        Claims claims = extractClaims(token, isAccessToken);
+        return claims.getSubject();
+    }
+
+    private Claims extractClaims(String token, boolean isAccessToken) {
+        try {
+            return Jwts
+                    .parser()
+                    .verifyWith(getSecretKey(isAccessToken ? accessTokenSecret : refreshTokenSecret))
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        }catch (Exception e) {
+            throw new IllegalArgumentException("Token is not valid or expired: " + e.getMessage());
+        }
     }
 }
