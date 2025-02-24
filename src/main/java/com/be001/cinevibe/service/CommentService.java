050fb7c10b4c5c6766e86d593a086ca6d7666a54
OzlamHasanova <7qk9bme@code.edu.az>
2025-02-24 13:02:23 +0400

    solve conflict

--- src/main/java/com/be001/cinevibe/service/CommentService.java
+++ src/main/java/com/be001/cinevibe/service/CommentService.java
@@ -1,9 +1,10 @@
 package com.be001.cinevibe.service;
 
+import com.be001.cinevibe.dto.CommentDTO;
+import com.be001.cinevibe.dto.CommentRequestDTO;
+import com.be001.cinevibe.exception.CommentNotFoundException;
 import com.be001.cinevibe.mapper.CommentMapper;
 import com.be001.cinevibe.model.Comment;
-import com.be001.cinevibe.dto.CommentDTO;
-import com.be001.cinevibe.exception.CommentNotFoundException;
 import com.be001.cinevibe.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -37,6 +38,33 @@ public class CommentService {
         return commentMapper.mapCommentToCommentDTO(comment);
     }
 
+    public CommentDTO update(Long id, CommentRequestDTO commentDTO) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new CommentNotFoundException("Comment not found"));
+        comment.setContent(commentDTO.getContent());
+        comment.setUpdatedAt(LocalDateTime.now());
+        commentRepository.save(comment);
+        return mapCommentToCommentDTO(comment);
+    }
 
+    public void delete(Long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new CommentNotFoundException("Comment not found"));
+        commentRepository.deleteById(comment.getId());
 
+    }
+
+    private Comment mapCommentDTOToComment(CommentDTO commentDTO) {
+        return Comment.builder()
+                .content(commentDTO.content())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    private CommentDTO mapCommentToCommentDTO(Comment comment) {
+        return CommentDTO.builder()
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .build();
+    }
 }
